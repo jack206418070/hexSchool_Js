daab4d535d94737736600f4f91b85809f9644b88
jack12081616 <jack90928@gmail.com>
2020-07-04 13:44:38 +0800

    add comment delete function

--- 2020-7-03/all.js
+++ 2020-7-03/all.js
@@ -3,7 +3,7 @@ let app = new Vue({
     data:{
         products:[
             {   
-                id: 1,
+                id: 1593826171806,
                 title: '活動白蝦',
                 category: '海鮮',
                 content: '活體冷洞,保證新鮮,甜味十足',
@@ -14,16 +14,24 @@ let app = new Vue({
                 price: 480,
                 unit: '盒',
                 options: {
-                    stars: 4,
-                    comments: [{
-                      name: '小明',
-                      avator: 'https://images.app.goo.gl/sSCJ6QwFgpZKKC1d8',
-                      comment: '吃了蝦子,到底要找小美還是漂亮阿姨'
-                    }]
+                    comments: [
+                        {
+                            id: 1,
+                            stars: 5,
+                            name: '小明',
+                            comment: '我喜歡吃阿姨煮的,也喜歡吃小美煮的'
+                        },
+                        {
+                            id: 2,
+                            stars: 1,
+                            name: '小美',
+                            comment: '小明吃過阿姨煮的,我只管給差評'
+                        },
+                    ]
                 }
             },
             {   
-                id: 2,
+                id: 1593826197355,
                 title: 'A菜',
                 category: '蔬菜水果',
                 content: '產地直送,保證新鮮',
@@ -34,18 +42,29 @@ let app = new Vue({
                 price: 20,
                 unit: '把',
                 options: {
-                    stars: 5,
-                    comments: [{
-                      name: '漂亮阿姨',
-                      avator: 'https://images.app.goo.gl/ZgZL85guTZyMsDkD9',
-                      comment: '今天煮青菜好呢,還是煮肉呢'
-                    }]
+                    comments: [
+                        {
+                            id: 1,
+                            stars: 5,
+                            name: '漂亮阿姨',
+                            comment: '今天煮青菜好呢,還是煮肉呢'
+                        },
+                        {
+                            id: 2,
+                            stars: 1,
+                            name: '小美',
+                            comment: '阿姨煮過的菜,我絕對不碰'
+                        },
+                    ]
                 }
             }
 
         ],
         tempProduct:{},
+        tempPos: 0,
+        tempComment:{},
         modalType:'',
+
     },
     methods:{
         openModal(type, product){
@@ -63,12 +82,18 @@ let app = new Vue({
                 case 'delete':
                     this.tempProduct = Object.assign({},product);
                     $('#delProductModal').modal('show');
+                    break;               
+                
+                case 'commet':
+                    this.tempProduct = Object.assign({},product);
+                    this.tempComment = this.tempProduct.comments[this.tempPos];
+                    $('#commetProductModal').modal('show');
                 default:
                 break;
             }
         },
         addProduct(){
-            this.tempProduct.id = this.products.length + 1;
+            this.tempProduct.id = new Date().getDate();
             this.products.push(this.tempProduct);
             this.tempProduct = {};
             $('#productModal').modal('hide');
@@ -92,6 +117,30 @@ let app = new Vue({
             })
             this.tempProduct = {};
             $('#delProductModal').modal('hide');
+        },
+        nextComment(){
+            if(this.tempPos === this.tempProduct.comments.length -1){
+                this.tempPos = 0
+            }else{
+                this.tempPos += 1;
+            }
+            this.tempComment = this.tempProduct.comments[this.tempPos];
+        },
+        preComment(){
+            if(this.tempPos === 0) {
+                this.tempPos = this.tempProduct.comments.length -1
+            }else{
+                this.tempPos -= 1;
+            }
+            this.tempComment = this.tempProduct.comments[this.tempPos];
+        },
+        deleteMessage(id){
+            this.tempProduct.comments.forEach((item, index) => {
+                if(item.id === id){
+                    this.tempProduct.comments.splice(index, 1);
+                }
+            })
+            this.tempComment = this.tempProduct.comments[0];
         }
         
     },
